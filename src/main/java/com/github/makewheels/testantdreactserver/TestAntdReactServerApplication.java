61ee55ddad2bb651c24f115c59b8b42720ada0a8
binqiao <you@example.com>
2022-01-21 16:32:45 +0800

    init

---
+++ src/main/java/com/github/makewheels/testantdreactserver/TestAntdReactServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.testantdreactserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAntdReactServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAntdReactServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/testantdreactserver/TestAntdReactServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.testantdreactserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAntdReactServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
